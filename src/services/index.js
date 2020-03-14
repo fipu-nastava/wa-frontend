e10f1b31520117c528142a8fe60a1547ecb81a12
Nikola Tanković <nikola.tankovic@gmail.com>
2020-03-14 19:48:33 +0100

    Week 3 services

---
+++ src/services/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+// instanca axios-a za potrebe Fipugram backenda
+let Service = axios.create({
+    baseURL: 'http://localhost:3000/',
+    timeout: 1000
+})
+
+// naš objekt za sve pozive koji se dotiču `Post`ova
+let Posts = {
+    add(post) {
+        return Service.post('/posts', post)
+    },
+    getAll(searchTerm) {
+        let options = {}
+
+        if (searchTerm) {
+            options.params = {
+                title: searchTerm
+            }
+        }
+
+        return Service.get('/posts', options)
+    }
+}
+
+export { Service, Posts } // exportamo Service za ručne pozive ili Posts za metode.
